1000ba0bca416a08c5e03566585bb9c359bf9ed0
phinic925 <phinicnyangate480@gmail.com>
2022-06-02 13:02:19 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    streetAddress: "nairobi street",
+    name:"phinic",
+   };
+   function updateEmployeeWithKeyAndValue(employee,key,value){
+       const newEmployee ={...employee};
+       newEmployee[key] = value;
+       return newEmployee;
+   
+}
+   
+   function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+       employee[key]= value;
+       return employee;
+
+   }
+   function deleteFromEmployeeByKey(employee,key){
+       const newEmployee=[key];
+       delete newEmployee[key];
+       return newEmployee;
+   }
+   function destructivelyDeleteFromEmployeeByKey(employee,key){
+       delete employee[key];
+       return employee;
+   }
